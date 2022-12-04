ddb8456a159431d0222e222874bb8d4c9a723323
SedenionProj <96470946+SedenionProj@users.noreply.github.com>
2022-12-04 17:26:23 +0100

    Update recherche_dfs.py

--- recherche_dfs.py
+++ recherche_dfs.py
@@ -19,11 +19,13 @@ def chercher_dfs(laby:nx.Graph, source:int = None, destination:int = None)->list
     """ Cherche le chemin le plus court entre les sommets source et destination
         selon le parcours en profondeur.
     """
+    # Initialisation des sommets source et destination s'ils ne sont pas renseignés
+    # Récupère la liste des sommets
     nodes = list(laby.nodes())
-    edges = list(laby.edges())
-
+    # Si source n'est pas renseigné, alors on impose source = 0
     if source == None:
         source = nodes[0]
+    # Si destination n'est pas renseigné, alors on impose destination = dernier sommet
     if destination == None:   
         destination = nodes[-1]
     
@@ -43,3 +45,21 @@ def chercher_dfs(laby:nx.Graph, source:int = None, destination:int = None)->list
             
 
     return visited
+
+if __name__ == "__main__":
+    # Création du labyrinthe de test
+    aretes = [(0, 1), (0, 4), (1, 0), (1, 5), (2, 6), (3, 7), (4, 0), (4, 5), (5, 1) , \
+     (5, 4), (5, 6), (5, 9), (6, 2), (6, 5), (6, 7), (6, 10), (7, 3), (7, 6), (8, 9),\
+     (9, 5), (9, 8), (9, 10), (10, 6), (10, 9), (10, 11), (11, 10)]
+    colonnes = 4
+    lignes = 3
+    nb_sommets = colonnes * lignes
+    noeuds = list(range(nb_sommets))
+    Labyrinthe = nx.Graph()
+    Labyrinthe.add_nodes_from(noeuds)
+    Labyrinthe.add_edges_from(aretes)
+    nx.draw(Labyrinthe)
+    # Lance le test de la fonction afficher_labyrinthe()
+    #afficher_labyrinthe(Labyrinthe, colonnes, lignes)
+    chemin_dfs =chercher_dfs(Labyrinthe)
+    print(chemin_dfs)
